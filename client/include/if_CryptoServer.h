d36566b3c0bb68fc5359f4d79075616d413470bb
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-08-28 18:33:37 +0200

    [NEW] SEOS-1608: Add C struct for CryptoServer store/load RPC interface

---
+++ client/include/if_CryptoServer.h
@@ -0,0 +1,20 @@
+/* Copyright (C) 2019-2020, Hensoldt Cyber GmbH */
+
+#pragma once
+
+#include "OS_Crypto.h"
+
+#include <stdint.h>
+
+typedef struct
+{
+    OS_Error_t (*storeKey)(OS_CryptoKey_Handle_t hKey, const char* name);
+    OS_Error_t (*loadKey)(OS_CryptoKey_Handle_t* hKey, unsigned int ownerId,
+                          const char* name);
+} if_CryptoServer_t;
+
+#define IF_CRYPTOSERVER_ASSIGN(_rpc_)   \
+{                                       \
+    .storeKey   = _rpc_ ## _storeKey,   \
+    .loadKey    = _rpc_ ## _loadKey,    \
+}
